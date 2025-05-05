a941f6d01589a96730269fe8d11cfdc0e58f72b4
Brandon98 <brandoncampbell98@hotmail.co.uk>
2025-05-05 13:36:29 +0000

    Add man-ubuntu tests

--- src/man-ubuntu/install.sh
+++ src/man-ubuntu/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get update -y
 
-if ! command -v unminimize 2>&1 >/dev/null
+if ! command -v unminimize &> /dev/null
 then
-    DEBIAN_FRONTEND=noninteractive apt-get install -y unminimize man-db
+    DEBIAN_FRONTEND=noninteractive apt-get install -y unminimize
 fi
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y man-db
